b70d1b0415d98ae4304eab0db1364c94b2bbfae6
Kristin198689 <kristin198689@gmail.com>
2024-10-31 14:09:06 +0100

    1

---
+++ date-format.js
@@ -0,0 +1,16 @@
+// variant1
+// const formattedDate1 = moment().format(`DD-MM-YYYY`)
+// const formattedDate2 = moment().format(`MMM Do YY`)
+// const formattedDate3 = moment().format(`dddd`)
+// console.log(formattedDate1)
+// console.log(formattedDate2)
+// console.log(formattedDate3)
+
+// variant2
+const now = moment();
+const formattedDate1 = now.format(`DD-MM-YYYY`);
+const formattedDate2 = now.format(`MMM Do YY`);
+const formattedDate3 = now.format(`dddd`);
+console.log(formattedDate1);
+console.log(formattedDate2);
+console.log(formattedDate3);
